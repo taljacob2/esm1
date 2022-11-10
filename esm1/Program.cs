1d6562a9e613dcc6bd6730d230936598bd1712b9
Tal Jacob <taljacob2@gmail.com>
2022-11-10 11:31:34 +0200

    Remove A Print From `Program.cs`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- esm1/Program.cs
+++ esm1/Program.cs
@@ -1,21 +1,7 @@
-﻿using esm1.Person;
-using esm1.Test;
+﻿using esm1.Test;
 
 Console.WriteLine(TestExecutor.Test1());
 Console.WriteLine(TestExecutor.Test2());
 Console.WriteLine(TestExecutor.Test3());
 Console.WriteLine(TestExecutor.Test4());
-//TestExecutor.Test5().Print();
 Console.WriteLine(TestExecutor.Test5());
-
-
-IPerson person = new PersonImpl()
-{
-    Id = 1,
-    FirstName = "John",
-    LastName = "Doe",
-    DateOfBirth = new DateTime(1990, 12, 26),
-    Height = 179
-};
-
-Console.WriteLine(person);
