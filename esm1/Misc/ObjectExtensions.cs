e8bc694b2b52e5155aa5ffab57026e97bf1367ef
Tal Jacob <taljacob2@gmail.com>
2022-11-10 11:20:39 +0200

    Add `ObjectExtensions` For Printing `ToString`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ esm1/Misc/ObjectExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Reflection;
+using System.Collections;
+
+namespace esm1.Misc
+{
+    public static class ObjectExtensions
+    {
+        private static int indentationLevel;
+
+        public static string ToStringExtension(this object obj)
+        {
+            StringBuilder stringBuilder = new();
+            int i = 0;
+            StringIndentation.NewLine(stringBuilder, indentationLevel);
+            stringBuilder.Append('{');
+            foreach (PropertyInfo property in obj.GetType().GetProperties())
+            {
+                if (property.GetType().GetProperties().Length > 0)
+                {
+                    indentationLevel++;
+                    StringIndentation.NewLine(stringBuilder,
+                        indentationLevel);
+                }
+
+                stringBuilder.Append(property.Name);
+                stringBuilder.Append(": ");
+                if (property.GetIndexParameters().Length > 0)
+                {
+                    stringBuilder.Append("Indexed Property cannot be used");
+                }
+                else
+                {
+                    stringBuilder.Append(property.GetValue(obj, null));
+                }
+
+                i++;
+                indentationLevel--;
+            }
+
+            StringIndentation.NewLine(stringBuilder, indentationLevel);
+            stringBuilder.Append('}');
+
+            return stringBuilder.ToString();
+        }
+
+        public static string ToStringEnumerableExtension(this IEnumerable enumerable)
+        {
+            StringBuilder stringBuilder = new();
+            stringBuilder.Append('[');
+            indentationLevel++;
+            foreach (var item in enumerable)
+            {
+                stringBuilder.Append(item.ToStringExtension());
+                stringBuilder.Append(',');
+            }
+            indentationLevel--;
+            StringIndentation.NewLine(stringBuilder, indentationLevel);
+            stringBuilder.Append(']');
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ esm1/Misc/StringIndentation.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace esm1.Misc
+{
+    public static class StringIndentation
+    {
+        public static void NewLine(StringBuilder stringBuilder,
+            int indentationLevel)
+        {
+            stringBuilder.Append(Environment.NewLine);
+            stringBuilder.Append(Create(indentationLevel));
+        }
+
+        public static string Create(int indentationLevel)
+        {
+            StringBuilder builder = new();
+            for (int i = 1; i <= indentationLevel; i++)
+            {
+                builder.Append("    ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
